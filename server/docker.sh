ba632827c77643bc8c26088acf4f0615d9160d80
CARLOSMARES <46010587+CARLOSMARES@users.noreply.github.com>
2024-04-15 03:31:49 -0600

    Se crea el archivo de docker y docker-composer.yml para crea los contenedores para nodejs y mongodb, se crea la estructura base de la app

---
+++ server/docker.sh
@@ -0,0 +1,3 @@
+# docker build -t server:1.0.0 .
+# docker run -d --name server -p 3000:3000 server:1.0.0
+docker-compose up -d --build
--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const db = require("./config/db");
 const router = express.Router();
 const port = process.env.PORT || 3000;
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1", router);
 router.use("/", require("./routes/routes"));
 router.get("/", (req, res) => {
